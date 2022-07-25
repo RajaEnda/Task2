34f5fb108ca5273b70ae909038c9a04c1e2e948a
raja <rajaenda521@gmail.com>
2022-07-25 22:24:19 +0700

    new

---
+++ components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="bg- z-0" style="background-color:blueviolet;">
+          <div class="flex items-center  mr-12 ml-24" >
+            <div style="height: 54px" class="pr-5 pt-2">
+              <p class="text-white text-2xl">Understand Everything About Vue JS Kelas B Oleh Raja Enda Pandapotan Ritonga</p>
+            </div>
+          </div>
+          </div>
+          
+</template>
+
+
---
+++ components/Sidebar.vue
@@ -0,0 +1,28 @@
+<template >
+<div class="w-60 h-full shadow-md bg-white px-1 absolute mt-8" style="background-color:aliceblue">
+  <ul class="relative" >
+    <li class="relative">
+      <a href="/" class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-black rounded hover:text-gray-900 hover:bg-gray-300 transition duration-300 ease-in-out" style="color:blue;">Vue Fundamental</a>
+
+    </li>
+        <li class="relative">
+      <a  href="cli" class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-black rounded hover:text-gray-900 hover:bg-gray-300 transition duration-300 ease-in-out" style="color:blue;">Vue CLI</a>
+      
+    </li>
+        <li class="relative">
+      <a href="ui" class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-black rounded hover:text-gray-900 hover:bg-gray-300 transition duration-300 ease-in-out" style="color:blue;">Vue UI Framework</a>
+      
+    </li>
+            <li class="relative">
+      <a href="nuxt" class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-black rounded hover:text-gray-900 hover:bg-gray-300 transition duration-300 ease-in-out" style="color:blue;">Vue With Nuxt</a>
+      
+    </li>
+            <li class="relative">
+      <a href="gql" class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-black rounded hover:text-gray-900 hover:bg-gray-300 transition duration-300 ease-in-out" style="color:blue;">Vue and Graph QL</a>
+      
+    </li>
+
+  </ul>
+</div>
+</template>
+
